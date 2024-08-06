074f02417b1b084c0dda82dfca34f1d8b7d65bc3
Alex Mutuku Mbolonzi <almbolonzi@gmail.com>
2024-08-06 20:16:01 +0300

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -9,7 +9,7 @@ const endpointMap = {
   prod: "https://api.hyperswitch.io/payments",
   sandbox: "https://sandbox.hyperswitch.io/payments",
   integ: "https://integ.hyperswitch.io/api/payments",
-  local: "https://sandbox.hyperswitch.io/payments", // Default or local environment endpoint
+  local: "http://localhost:9050", // Default or local environment endpoint
 };
 
 const backendEndPoint = endpointMap[sdkEnv] || endpointMap.local;
